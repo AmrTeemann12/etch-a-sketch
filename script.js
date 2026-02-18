76a46770624fb0143d8eec0dff026fa651c6712c
amr_khaled <160667190+AmrTeemann12@users.noreply.github.com>
2026-02-18 18:23:34 +0200

    add the logic to etch the sketch. added the etch function to change the background color of the grid squares when passing the mouse through while pressing the mouse button. added set squares buttons to reset the number of squares.

--- script.js
+++ script.js
@@ -12,7 +12,34 @@ function createGrid(n){
         square.style.width = `${squareWidthPercentage}%`
 
         container.appendChild(square)
-    }   
+    }
+    etch()
 }
 
 createGrid(16)
+
+function etch(){
+    let isMouseDown = false;
+    document.body.addEventListener("mousedown", (e) =>{
+        e.preventDefault()
+        isMouseDown = true;
+    });
+    document.addEventListener("mouseup", () => isMouseDown = false);
+    document.addEventListener("mouseleave", () => isMouseDown = false);
+    const squares = document.querySelectorAll(".square-border");
+
+    squares.forEach(square => square.addEventListener("mouseenter", (e) =>{
+        e.preventDefault()
+        if (isMouseDown){
+        e.target.style.backgroundColor = "black"
+        }
+    }));
+}
+
+const set = document.querySelector("#set-grid");
+
+set.addEventListener("click", (e) =>{
+    e.preventDefault();
+    container.innerHTML="";
+    createGrid(+prompt("pick a number for squares per side."), 10)
+})
